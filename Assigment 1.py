be7b0ed4ac19dce5c62738eaf72199d4fc4f037d
Shweta8530 <95363045+Shweta8530@users.noreply.github.com>
2021-12-02 16:51:03 +0530

    Add files via upload

---
+++ Assigment 1.py
@@ -0,0 +1,38 @@
+A="S"
+B="h"
+C="w"
+D="e"
+E="t"
+F="a"
+print("ASCII value of A is",ord(A))
+print("ASCII value of B is",ord(B))
+print("ASCII value of C is",ord(C))
+print("ASCII value of D is",ord(D))
+print("ASCII value of E is",ord(E))
+print("ASCII value of F is",ord(F))
+
+
+print("**********************")
+
+
+A="iND@IA"
+print("The ASCII number of A is",ord("A"))
+
+
+
+print("**********************")
+
+
+
+A=80
+print(chr(A))
+B=92
+print(chr(B))
+C=99
+print(chr(C))
+D=106
+print(chr(D))
+E=116
+print(chr(E))
+
+
